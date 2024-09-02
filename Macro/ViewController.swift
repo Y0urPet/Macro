f82af58afa6250c467f760bb3fe13a93d4c95415
timothy <tandrian24@bsd.idserve.net>
2024-09-02 16:37:27 +0700

    Initial Commit

---
+++ Macro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Macro
+//
+//  Created by Timothy Andrian on 02/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
